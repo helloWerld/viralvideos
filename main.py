8cd1d5dfc9c81b8e3dfd473f1575e0516783f8ff
SolPyDev <108149462+SolPyDev@users.noreply.github.com>
2022-08-24 13:04:12 -0400

    name main

--- main.py
+++ main.py
@@ -61,33 +61,35 @@ def subs(chan_id):
     response = request.execute()
     return response
 
-#Calling the YouTube API search function
-response = youtube_search(MAX_RESULTS)
 
-#Print results
-print("VIEW RATIO | VIEWS | SUBSCRIBERS | CHANNEL NAME | TITLE | VIDEO ID | CHANNEL ID")
+if __name__ == '__main__':
+    #Calling the YouTube API search function
+    response = youtube_search(MAX_RESULTS)
 
-for i in range(0, len(response['items'])-1):
-    try:
-        # Get video and channel stats from youtube search object
-        video_id = response['items'][i]['id']['videoId']
-        video_title = response['items'][i]['snippet']['title']
-        channel_id = response['items'][i]['snippet']['channelId']
-        channel_name = response['items'][i]['snippet']['channelTitle']
+    #Print results
+    print("VIEW RATIO | VIEWS | SUBSCRIBERS | CHANNEL NAME | TITLE | VIDEO ID | CHANNEL ID")
 
-        # Get video views from youtube video object
-        vv_response = vid_views(video_id)
-        video_views = int(vv_response['items'][0]['statistics']['viewCount'])
+    for i in range(0, len(response['items'])-1):
+        try:
+            # Get video and channel stats from youtube search object
+            video_id = response['items'][i]['id']['videoId']
+            video_title = response['items'][i]['snippet']['title']
+            channel_id = response['items'][i]['snippet']['channelId']
+            channel_name = response['items'][i]['snippet']['channelTitle']
 
-        # Get subscribers count from youtube channel object
-        subs_response = subs(channel_id)
-        subscribers = subs_response['items'][0]['statistics']['subscriberCount']
+            # Get video views from youtube video object
+            vv_response = vid_views(video_id)
+            video_views = int(vv_response['items'][0]['statistics']['viewCount'])
 
-        # Math to find viral videos
-        view_ratio = round(int(video_views) / int(subscribers), 2)
+            # Get subscribers count from youtube channel object
+            subs_response = subs(channel_id)
+            subscribers = subs_response['items'][0]['statistics']['subscriberCount']
 
-        if video_views > 5000 and view_ratio > 0:
-            print(
-                f"{view_ratio} | {video_views} | {subscribers} | {channel_name} | Title: {video_title} | VideoID: {video_id} | ChannelID: {channel_id}")
-    except:
-        print("YouTube Data API timeout")
+            # Math to find viral videos
+            view_ratio = round(int(video_views) / int(subscribers), 2)
+
+            if video_views > 5000 and view_ratio > 0:
+                print(
+                    f"{view_ratio} | {video_views} | {subscribers} | {channel_name} | Title: {video_title} | VideoID: {video_id} | ChannelID: {channel_id}")
+        except:
+            print("YouTube Data API timeout")
